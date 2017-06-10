49f37d7162259e4a09b62749a4557ee2cfede487
Lover Almost <Lover Almost>
2017-06-10 14:51:20 +0800

    [xurao]number

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /**
  * Created by Asus on 2017/6/10.
  */
-var app = new PIXI.Application(window.innerWidth, window.innerHeight, {backgroundColor: 0x1099bb});
+var app = new PIXI.Application(window.innerWidth, window.innerHeight, {backgroundColor: 0xFFF8DC});
 document.body.appendChild(app.view);
 
 var basicText = new PIXI.Text('2048',{
     fontSize:100,
-    fill:0xEBE0D0
+    fill:0xC7A97C
 });
 basicText.anchor.set(0.5);
 basicText.x = app.renderer.width/2;
@@ -23,15 +23,41 @@ for(var i = 0;i<4;i++)
 //#B38A4D
 
 
+var graphics = new PIXI.Graphics();
+graphics.beginFill(0xC7A97C, 1);
+graphics.drawRect(app.renderer.width/10-5, app.renderer.height/8*3-5,305, 305);
+app.stage.addChild(graphics);
+
+
+
 for(var i = 0;i<4;i++)
 {
    for(var j = 0;j<4;j++)
    {
        var graphics = new PIXI.Graphics();
-
-       graphics.beginFill(0xEBE0D0, 1);
+       graphics.beginFill(0xFFEBCD, 1);
        graphics.drawRect(app.renderer.width/10+j*75, app.renderer.height/8*3+i*75 ,70, 70);
        app.stage.addChild(graphics);
    }
+}
+var x = generateRandomNumber();
+var y = generateRandomNumber();
 
+var graphics = new PIXI.Graphics();
+graphics.beginFill(0xFFFAF0, 1);
+graphics.drawRect(app.renderer.width/10+x*75, app.renderer.height/8*3+y*75 ,70, 70);
+app.stage.addChild(graphics);
+
+var number = new PIXI.Text('2',{
+    fontSize:50,
+    fill:0xC7A97C
+});
+number.anchor.set(0.5);
+number.x = app.renderer.width/10+35+x*75;
+number.y =  app.renderer.height/8*3+35+y*75;
+app.stage.addChild(number);
+
+function generateRandomNumber() {
+
+    return Math.floor(Math.random()*4);
 }
